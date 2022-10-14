c4c09472e21d95d24b3f686d71ab899816f9d00a
unknown <emansarwar3@gmail.com>
2022-10-14 15:39:47 +0600

    hr add

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,6 +5,7 @@ import { NavLink } from 'react-router-dom';
 const Footer = () => {
     return (
         <div>
+            <hr/>
             <h4>To know details about RICH QUALITY  <NavLink className="link-about" to='/about'>CLICK ME</NavLink></h4>
             <h4><small>© 2022 Envato Pty Ltd. Trademarks and brands are the property of their respective owners.</small></h4>
         </div>
